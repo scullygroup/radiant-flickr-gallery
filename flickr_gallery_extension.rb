4006449f0aca1b30eb731f6f0aae5f9c7b2ae01a
Nathan Batson <nathanbatson@nathan-batsons-ep45-ud3r.local>
2010-06-01 23:04:14 -0400

    removed all unnecessary bullshit

--- flickr_gallery_extension.rb
+++ flickr_gallery_extension.rb
@@ -6,7 +6,7 @@ require 'nokogiri'
 
 class FlickrGalleryExtension < Radiant::Extension
   version "1.0"
-  description "A Radiant extension that allows you to add paginated flickr galleries"
+  description "A Radiant extension that allows you to add simple flickr galleries"
   url "http://www.scullytown.com"
   
   define_routes do |map|
@@ -15,21 +15,8 @@ class FlickrGalleryExtension < Radiant::Extension
     #map.connect 'testimonials', :url => '/testimonials', :controller => "site", :action => "show_page"
     #map.connect '', :controller => "testimonials", :action => "index"
    
-    # map.with_options(:controller => 'flickr_pages') do |flickr|
-    #   flickr.flickr_page_index '/guestphotos', :action => 'index'
-    # end
-    # 
-    
     map.with_options(:controller => 'admin/gallery') do |flickr|
       flickr.flickr_gallery_index   'admin/gallery', :action => 'index'
-    end
-    
-    map.with_options(:controller => 'flickr_gallery') do |flickr|
-      flickr.flickr_gallery_photoset '/flickr-gallery/:setid', :action => 'photoset'
-    end
-    
-    map.with_options(:controller => 'flickr_javascript') do |flickr|
-      flickr.flickr_gallery_photoset '/flickr-gallery/:setid/javascript', :action => 'photoset'
     end
 
   end
--- lib/flickr_gallery_tags.rb
+++ lib/flickr_gallery_tags.rb
@@ -7,16 +7,12 @@ module FlickrGalleryTags
     photosetarray = attr[:photosets].split(',')
     
     @photos = get_photos(photosetarray[0])
-    @photos_json = @photos.to_json
-    
     
     @hastitle = false
     
     if !attr[:title].nil?
       @hastitle = true
     end
-    
-    # @photosets = get_photosets(photosetarray)
     
     # testing parsing templates from tags
     parse_template 'flickr_gallery/_gallery'
@@ -31,96 +27,6 @@ module FlickrGalleryTags
         template = f.read
       }
       ERB.new(template).result(binding)
-    end
- 
-    def get_photosets(sets)
-      require 'open-uri'
-      require 'nokogiri'
-      
-      config = YAML.load_file("config/flickr.yml")
-      @api_key = config['key']
-      @user = config['user']
-
-      @doc = Nokogiri::XML(open("http://api.flickr.com/services/rest/?method=flickr.photosets.getList&api_key=#{@api_key}&user_id=#{@user}"))
-
-      first = true
-      flickr_photosets = []
-
-      sets.each do |setid|
-        @doc.xpath("/rsp/photosets/photoset").each do |node|
-          
-          # if the photoset result for the user
-          if setid == (node['id'])
-            title = node.search("title").inner_html
-          
-            # get the first photo in the set for the primary photo
-            @photoset_photos = Nokogiri::XML(open("http://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=#{@api_key}&photoset_id=#{node['id']}&per_page=1"))
-            first_photo = @photoset_photos.xpath("/rsp/photoset/photo").first
-        
-            # now take that id and find me the actual photo
-            @set_photo = Nokogiri::XML(open("http://api.flickr.com/services/rest/?method=flickr.photos.getSizes&api_key=#{@api_key}&photo_id=#{first_photo['id']}"))
-        
-            set_photo = ""
-        
-            @set_photo.xpath("/rsp/sizes/size").each do |size|
-              if size['label'] == "Square"
-                set_photo = size['source']
-              end
-            end
-        
-            a_class = ""
-        
-            if !params[:setid] && first
-              a_class = "current"
-            elsif params[:setid] == node['id']
-              a_class = "current"
-            end
-        
-            flickr_photosets.push("<a class=\"#{a_class}\" href=\"/flickr-gallery/#{node['id']}\"><img src=\"#{set_photo}\" /><span>#{title.text}</span></a>")
-        
-            # its not the first run through
-            first = false
-          end
-        end
-      end
-
-      # @doc.xpath("/rsp/photosets/photoset").each do |node|
-      #   
-      #   # if the photoset result for the user
-      #   if sets.include?(node['id'])
-      #     title = node.children
-      #   
-      #     # get the first photo in the set for the primary photo
-      #     @photoset_photos = Nokogiri::XML(open("http://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=#{@api_key}&photoset_id=#{node['id']}&per_page=1"))
-      #     first_photo = @photoset_photos.xpath("/rsp/photoset/photo").first
-      # 
-      #     # now take that id and find me the actual photo
-      #     @set_photo = Nokogiri::XML(open("http://api.flickr.com/services/rest/?method=flickr.photos.getSizes&api_key=#{@api_key}&photo_id=#{first_photo['id']}"))
-      # 
-      #     set_photo = ""
-      # 
-      #     @set_photo.xpath("/rsp/sizes/size").each do |size|
-      #       if size['label'] == "Square"
-      #         set_photo = size['source']
-      #       end
-      #     end
-      # 
-      #     a_class = ""
-      # 
-      #     if !params[:setid] && first
-      #       a_class = "current"
-      #     elsif params[:setid] == node['id']
-      #       a_class = "current"
-      #     end
-      # 
-      #     flickr_photosets.push("<a class=\"#{a_class}\" href=\"/flickr-gallery/#{node['id']}\"><img src=\"#{set_photo}\" /><span>#{title.text}</span></a>")
-      # 
-      #     # its not the first run through
-      #     first = false
-      #   end
-      # end
-
-      return flickr_photosets
     end
     
     def get_photos(setid)
@@ -189,19 +95,6 @@ module FlickrGalleryTags
       
       return results
 
-    end
-
-    def get_description(photo_id)
-      require 'open-uri'
-      require 'nokogiri'
-      
-      config = YAML.load_file("config/flickr.yml")
-      api_key = config['key']
-
-      photo_info = Nokogiri::XML(open("http://api.flickr.com/services/rest/?method=flickr.photos.getInfo&api_key=#{api_key}&photo_id=#{photo_id}"))
-      description = photo_info.xpath("/rsp/photo/description").first
-
-      return description.text
     end
  
 end
